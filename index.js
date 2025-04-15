7d77db125b266886ecd49fbc7e95bbf29a941320
marg4ryn <poweski.jan@gmail.com>
2025-04-15 23:54:50 +0200

    final

--- index.js
+++ index.js
@@ -4,36 +4,30 @@ const { loadSchemaSync } = require('@graphql-tools/load');
 const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 const path = require('path');
+
+const connectDB = require('./config/db');
 const resolvers = require('./resolvers/resolvers');
 
-const schema = loadSchemaSync(path.join(__dirname, 'schema.graphql'), {
+connectDB();
+
+const typeDefs = loadSchemaSync(path.join(__dirname, 'schema/schema.graphql'), {
   loaders: [new GraphQLFileLoader()],
 });
 
-const executableSchema = makeExecutableSchema({
-  typeDefs: schema,
+const schema = makeExecutableSchema({
+  typeDefs,
   resolvers,
 });
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-  schema: executableSchema,
+  schema,
   graphiql: true,
 }));
 
-app.listen(4000, () => {
-  console.log('Server działa na http://localhost:4000/graphql');
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`The server is running on http://localhost:${PORT}/graphql`);
 });
-
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/graphql-todo', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.connection.once('open', () => {
-  console.log('✅ Połączono z MongoDB');
-});
-
